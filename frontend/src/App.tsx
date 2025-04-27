efe53ab949b42ebc26b9301b4361ddc34b06b874
HungNg <21130367@st.hcmuaf.edu.vn>
2025-04-27 14:27:58 +0700

    payment & payment-success page FE

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,8 @@ import About from './pages/static/About';
 import Contact from './pages/static/Contact';
 import SearchResults from './pages/SearchResults';
 import RegisteredCourses from './pages/RegisteredCourses';
+import PaymentMethod from './pages/PaymentMethod';
+import PaymentSuccess from './pages/PaymentSuccess';
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
         <Route path="/contact" element={<Contact />} />
         <Route path="/search" element={<SearchResults />} />
         <Route path="/my-courses" element={<RegisteredCourses />} />
+        <Route path="/payment/:courseId" element={<PaymentMethod />} />
+        <Route path="/payment/success" element={<PaymentSuccess />} />
       </Routes>
     </Router>
   );
