12ebfefe81a06d49e4f272a6869cb6de8627ba04
HungNg <21130367@st.hcmuaf.edu.vn>
2025-06-23 03:44:54 +0700

    xem danh sách khóa học

--- backend/src/main/java/org/nlu/backend/service/enrollment/IEnrollmentService.java
+++ backend/src/main/java/org/nlu/backend/service/enrollment/IEnrollmentService.java
@@ -1,7 +1,11 @@
 package org.nlu.backend.service.enrollment;
 
 import org.nlu.backend.dto.request.enrollment.CourseEnrollmentRequest;
+import org.nlu.backend.dto.response.course.EnrolledCourseResponse;
+
+import java.util.List;
 
 public interface IEnrollmentService {
     void enrollCourse(Long userId, CourseEnrollmentRequest request);
+    List<EnrolledCourseResponse> getEnrolledCourses();
 }
